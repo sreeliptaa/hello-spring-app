463a3a1eb5bd7423ab46191e6ec72415e220ce6e
sreeliptaa <sreelipta65@gmail.com>
2021-12-01 03:54:26 +0530

    Added : Code to display the hello message by using HelloRestController

---
+++ src/main/java/com/bridgelabz/controller/HelloRestController.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Purpose: Class is implemented for handling the request from the user
+ * @RestController defines that it will deal with the rest controller
+ * @RequestMapping will handle the request
+ *
+ * @author : SREELIPTA
+ * @since 01-12-2021
+ */
+
+@RestController
+public class HelloRestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Bridgelabz!";
+    }
+}
--- src/main/java/com/bridgelabz/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/controller/HelloWebController.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Purpose: Class is implemented for handling the request from the user
+ *
+ * @author : SREELIPTA
+ * @since 01-12-2021
+ */
+
 @Controller
 public class HelloWebController {
     @GetMapping("/web")
