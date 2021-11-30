cbb0e2d78f433fed5018c6b09496b44beb4850e4
sreeliptaa <sreelipta65@gmail.com>
2021-12-01 01:55:34 +0530

    Added : Project files from spring initialiser

---
+++ src/main/java/com/bridgelabz/HelloSpringAppApplication.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Purpose: To Simulate the Hello Spring App
+ *
+ * @author : SREELIPTA
+ * @since 01-12-2021
+ */
+
+
+@SpringBootApplication
+public class HelloSpringAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloSpringAppApplication.class, args);
+    }
+
+}
